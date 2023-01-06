9cd3b0fc09c93179f8089277dd5caec0877596ba
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-01-06 20:27:43 +0100

    fixed and iteration 5 WIP.

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,12 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(moviesArray) {
-  let newArray = moviesArray.map((movies) => movies.director)
+  let newArray = moviesArray.map((element) => element.director)
+  // console.log(newArray)
+
   return newArray
-  //return newArray.sort()
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
@@ -13,19 +15,46 @@ function howManyMovies(moviesArray) {
     (movies) =>
       movies.director === "Steven Spielberg" && movies.genre.includes("Drama")
   )
+  //console.log(spielbergDramaMovies)
   return spielbergDramaMovies.length
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+
 function scoresAverage(moviesArray) {
-  if (moviesArray.length == 0) return null
+  if (moviesArray.length == 0) {
+    return 0
+  }
+  let avarageScore = moviesArray.reduce((acc, newScore) => {
+    if (!newScore.score) {
+      return acc
+    } else return acc + newScore.score / moviesArray.length
+  }, 0)
+  //console.log(avarageScore)
+  return Math.round(avarageScore * 100) / 100
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const allDramaMovies = moviesArray.filter((drama) =>
+    drama.genre.includes("Drama")
+  )
+  //console.log(scoresAverage(allDramaMovies))
+  return scoresAverage(allDramaMovies)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  let yearsArray = moviesArray
+    .map((element) => element.year)
+    .sort((a, b) => a - b)
+
+  const mySet = new Set(yearsArray)
+  const finalArray = Array.from(mySet)
+
+  console.log(finalArray)
+  return finalArray
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
